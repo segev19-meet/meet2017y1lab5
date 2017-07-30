4846603df2bf9d98f6b949386e6eb24307dcc808
segev19-meet <segev19@meet.mit.edu>
2017-07-30 18:22:23 +0300

    Stopping here

---
+++ lists.py
@@ -0,0 +1,15 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> dog='dalmatian'
+>>> len(dog)
+9
+>>> dogs=[dog,'poodle''boxer']
+>>> len(dogs)
+2
+>>> print(x+y+z)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(x+y+z)
+NameError: name 'x' is not defined
+>>> 
